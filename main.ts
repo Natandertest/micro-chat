9606f06a881ab4bba9a1243d75e453661d297853
Natandertest <117825647+Natandertest@users.noreply.github.com>
2022-11-10 13:05:36 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    radio.setGroup(1)
+    radio.sendString("1")
+    basic.showString("HACKED X_X")
+})
+radio.onReceivedString(function (receivedString) {
+	
+})
+basic.forever(function () {
+	
+})
